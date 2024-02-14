d7ef2d790acbcd947123d7268f6437b4843c41eb
whyzotee <zenlektomyum@gmail.com>
2024-02-14 14:02:13 +0700

    chore: add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Providers from "./providers";
 export const metadata: Metadata = {
   title: "Hello World!",
   description: "Make a move, Make a difference.",
+  name: "google-site-verification",
+  content: "yWJFeFRMKfPWyKghAPGMn_5g0HoTwQYTGcTK8S22oSo",
 };
 
 export default function RootLayout({
